60bbdfcffc6ac9b036b7a8128115c9b0da79dfdc
Yinan Zhu <zynsteve@tamu.edu>
2017-11-26 23:05:42 -0600

    Update HttpProxy.h

--- Proxy Server/HttpProxy.h
+++ Proxy Server/HttpProxy.h
@@ -1,66 +1,98 @@
-#include<string>
 #include<vector>
+#include<string>
 #include<time.h>
+
+
 
 using namespace std;
 
+
+
 #define MAX_CACHE_SIZE 10
+
 #define STDIN 0
 
-class Header
-{
-  public:
-  Header();
+
+
+class Header {
+public:
+    Header();
 };
 
-class EntityHeader : public Header
-{
-  string contentType;
-  string contentEncoding;
-  string expires;
-  string lastModified;
-  string date;
-  time_t lastAccessed;
 
-  public:
-  EntityHeader();
-  void setContentType(string);
-  void setContentEncoding(string);
-  void setExpires(string);
-  void setLastModified(string);
-  void setDate(string);
-  void setLastAccessed(time_t);
-  string getContentType();
-  string getContentEncoding();
-  string getExpires();
-  string getLastModified();
-  string getDate();
-  time_t getLastAccessed();
-  string toString();
+
+class Entity_Header : public Header {
+
+    string content_type;
+
+    string content_encoding;
+
+    string expires;
+
+    string last_modified;
+
+    string date;
+
+    time_t last_accessed;
+
+public:
+    Entity_Header();
+    void set_content_type(string);
+    void set_content_encoding(string);
+
+    void set_expires(string);
+    void set_last_modified(string);
+    void set_date(string);
+    void set_last_accessed(time_t);
+
+    string get_content_type();
+    string get_content_encoding();
+
+    string get_expires();
+    string get_last_modified();
+    string get_date();
+    time_t get_last_accessed();
+
+    string to_string();
+
 };
 
-class Entity
-{
-  EntityHeader* header;
-  string body;
 
-  public:
-  Entity();
-  void setHeader(EntityHeader*);
-  void setBody(string);
-  EntityHeader* getHeader();
-  string getBody();
-  string toString();
+
+class Entity {
+
+    Entity_Header* header;
+    string body;
+
+public:
+    Entity();
+
+    void set_header(Entity_Header *);
+    void set_body(string);
+
+    Entity_Header* get_header();
+
+    string get_body();
+    string to_string();
+
 };
 
-//methods
-string extractRequestUri(char*);
-bool isGetReq(char*);
-//Entity* parseResponse(string, int);
-Entity* parseResponse(string);
-vector<string> strsplit(string, string);
-double timeDiff(string, string);
-time_t toTimeT(string);
-string fromTimeT(time_t);
-time_t getCurrentTime();
-Entity* stampPage(Entity*);
+
+
+bool is_get_req(char *);
+
+string extract_uri(char *);
+
+Entity* parse_response(string);
+
+double timediff(string, string);
+
+vector<string> string_split(string, string);
+
+time_t to_Time_T(string);
+
+string from_Time_T(time_t);
+
+Entity* stamp_page(Entity *);
+
+time_t get_current_time();
